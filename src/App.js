1416af893c6917d37af9cfda86321bf39632a6b2
Subham Sahu <83798604+drcount-root@users.noreply.github.com>
2023-01-18 12:46:04 +0000

    18012023-18:16

--- src/App.js
+++ src/App.js
@@ -39,8 +39,8 @@ const theme = createTheme({
 });
 export const config = {
   //Mock server api
-  // endpoint: 'https://937eb2d1-2b0f-42e1-af80-2c776b32bcc1.mock.pstmn.io/v1/videos'
-  endpoint: "https://xflixbackendapp.herokuapp.com/v1/videos",
+  // endpoint: "https://cf1ff9e4-a87b-4997-b857-40a77e875169.mock.pstmn.io/v1/videos",
+  endpoint: "https://xflix-mpt8.onrender.com/v1/videos",
 };
 
 
